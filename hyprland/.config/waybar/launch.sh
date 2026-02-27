ad8d1b0d82d53efab01aaec3d8dcc93878557fda
clippyricer <bannansmooth68@outlook.com>
2026-02-27 13:56:32 -0500

    update

--- hyprland/.config/waybar/launch.sh
+++ hyprland/.config/waybar/launch.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-killall waybar
-killall dunst
+killall -q waybar
+killall -q dunst
 
+while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
+
+export PATH=$PATH:$HOME/.local/bin:$HOME/.cargo/bin:/usr/local/bin
+
+# 4. Launch waybar and log errors to a file so we can see what's happening
 waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
 dunst &
