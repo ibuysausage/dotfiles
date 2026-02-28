866008145331e87bb64640ac8397ee7421debb42
clippyricer <bannansmooth68@outlook.com>
2026-02-27 20:43:56 -0500

    update wallpaper.sh

--- hyprland/.config/hypr/wallpaper.sh
+++ hyprland/.config/hypr/wallpaper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-killall -q awww-daemon
+killall -q awww-daemon; rm -rf ~/.cache/awww/
 
 awww-daemon &
-
 awww img $HOME/Pictures/Wallpapers/arch.jpg --transition-type wipe --transition-angle 30 
