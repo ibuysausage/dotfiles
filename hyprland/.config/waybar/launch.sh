0a794606e43880961f69e151575c9d5d5ff7f776
clippyricer <bannansmooth68@outlook.com>
2026-03-01 14:48:30 -0500

    update launch.sh

--- hyprland/.config/waybar/launch.sh
+++ hyprland/.config/waybar/launch.sh
@@ -7,6 +7,9 @@ while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
 
 export PATH=$PATH:$HOME/.local/bin:$HOME/.cargo/bin:/usr/local/bin
 
-# 4. Launch waybar and log errors to a file so we can see what's happening
+WIDTH=$(hyprctl monitors -j | jq '.[0].width')
+WIDTH=$((WIDTH - 20))
+sed -i "s/\"width\": .*/\"width\": $WIDTH,/" ~/.config/waybar/config.jsonc
+
 waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
 dunst &
